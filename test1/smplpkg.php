b63115660f35df733310d986b51953955d87a39d
Hiroshi Sakurai <sakurai777@gmail.com>
2015-11-15 14:06:08 +0900

    Update pkgs

--- test1/smplpkg.php
+++ test1/smplpkg.php
@@ -3,8 +3,8 @@
 class test1_smplpkg extends smplpkg {
     public $depends = array();
     function install($argv) {
-      system("cd caches; git clone http://github.com/hsk/smplpkg_test1");
-      system("cd caches/smplpkg_test1; git pull http://github.com/hsk/smplpkg_test1");
+      system("git clone http://github.com/hsk/smplpkg_test1 test1");
+      system("cd test1; git pull http://github.com/hsk/smplpkg_test1");
       echo "install test1\n";
     }
 }
