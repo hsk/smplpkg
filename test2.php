441f170812db47e44b50eefa8e7da27f33df3f40
Hiroshi Sakurai <sakurai777@gmail.com>
2015-11-15 18:59:47 +0900

    Update test2.php

--- test2.php
+++ test2.php
@@ -2,7 +2,7 @@
 
 class test2 extends smplpkg {
   var $zip = "https://github.com/hsk/smplpkg_test2/archive/test2-0.0.1.zip";
-  public $comment = "this is test2 class library";
-  public $depends = array("test1");
-  public $repo = "git@github.com:hsk/smplpkg_test2.git";
+  var $comment = "this is test2 class library";
+  var $depends = array("test1");
+  var $repo = "git@github.com:hsk/smplpkg_test2.git";
 }
